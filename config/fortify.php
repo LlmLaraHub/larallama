f75a23f5f1f019478f70b6dea2939c5e17d1244a
alnutile <alfrednutile@gmail.com>
2024-03-14 11:00:42 -0400

    fix the ui

--- config/fortify.php
+++ config/fortify.php
@@ -144,7 +144,7 @@ return [
     */
 
     'features' => [
-        Features::registration(),
+        //Features::registration(),
         Features::resetPasswords(),
         // Features::emailVerification(),
         Features::updateProfileInformation(),
