a85169f678ad69473cd7ddfd48db88c3613f117c
Alfred Nutile <alfrednutile@gmail.com>
2024-03-28 08:56:07 -0400

    add reverb

---
+++ resources/js/Components/SecondaryLink.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+defineProps({
+    type: {
+        type: String,
+        default: 'inertia',
+    },
+    href: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <Link 
+    v-if="type === 'inertia'"
+    :href="href" class="inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150">
+        <slot />
+    </Link>
+</template>
--- resources/js/Pages/Collection/Chat.vue
+++ resources/js/Pages/Collection/Chat.vue
@@ -2,12 +2,14 @@
 import AppLayout from '@/Layouts/AppLayout.vue';
 import Welcome from '@/Components/Welcome.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
+import SecondaryLink from '@/Components/SecondaryLink.vue';
 import { computed, onMounted, provide, ref } from 'vue';
 
 import { useDropzone } from "vue3-dropzone";
 import { router, useForm } from '@inertiajs/vue3';
 import FileUploader from './Components/FileUploader.vue';
 import ChatUi from '@/Pages/Chat/ChatUi.vue';
+import { DocumentTextIcon } from '@heroicons/vue/24/outline';
 const props = defineProps({
     collection: {
         type: Object,
@@ -45,20 +47,26 @@ provide('system_prompt', props.system_prompt);
 
 
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-
-
                     <!-- Top area -->
 
                     <div class="border-b pb-5 px-3 py-4">
-                        <h3 class="text-base font-semibold leading-6 text-gray-900">{{ collection.data.name }}</h3>
-                        <p class="mt-2 max-w-4xl text-sm text-gray-500">
-                            {{ collection.data.description }}
-                        </p>
+                        <div class="flex justify-between items-center">
+                        <div>
+                            <h3 class="text-base font-semibold leading-6 text-gray-900">{{ collection.data.name }}</h3>
+                            <p class="mt-2 max-w-4xl text-sm text-gray-500">
+                                {{ collection.data.description }}
+                            </p>
+                        </div>
+                        <SecondaryLink class="flex justify-between items-center gap-4" :href="route('collections.show', {
+                    collection: collection.data.id,
+                })">    <DocumentTextIcon class="h-5 w-5"></DocumentTextIcon>
+                                
+                            Back to Documents
+                        </SecondaryLink>
+                    </div>
                     </div>
                     <div class="p-10">
-                       <ChatUi 
-                       :chat="chat" 
-                       :messages="messages"></ChatUi>
+                        <ChatUi :chat="chat" :messages="messages"></ChatUi>
                     </div>
                 </div>
             </div>
