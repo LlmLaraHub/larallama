32f1345676427c15a967681fe4d8a9b75c87a810
Alfred Nutile <alfrednutile@gmail.com>
2024-05-17 09:15:39 -0400

    fix migration

---
+++ database/migrations/2024_05_17_120021_oops_on_filters.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+
+        Schema::drop("collection_filter");
+
+        Schema::create('document_filter', function (Blueprint $table) {
+            $table->foreignId('filter_id')->constrained()->onDelete('cascade');
+            $table->unsignedBigInteger('document_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
