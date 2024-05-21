1fceb8b6fde79778bcd3238eb16a7efe1aae83b6
Alfred Nutile <alfrednutile@gmail.com>
2024-05-21 15:27:33 -0400

    add the postmark library

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,11 +10,3 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  */
 
 import './echo';
-
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allow your team to quickly build robust real-time web applications.
- */
-
-import './echo';
