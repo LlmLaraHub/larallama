7ed267ff68403f5b2b450d661a923adbf65484fd
Alfred Nutile <alfrednutile@gmail.com>
2024-06-20 10:38:54 -0400

    add model default

--- config/llmdriver.php
+++ config/llmdriver.php
@@ -32,8 +32,8 @@ return [
             'api_url' => env('OPENAI_API_URL', 'https://api.openai.com/v1'),
             'models' => [
                 'embedding_model' => env('OPENAI_EMBEDDING_MODEL', 'text-embedding-3-large'),
-                'completion_model' => env('OPENAI_COMPLETION_MODEL', 'gpt-4-turbo-preview'),
-                'chat_model' => env('OPENAICHAT_MODEL', 'gpt-4-turbo-preview'),
+                'completion_model' => env('OPENAI_COMPLETION_MODEL', 'gpt-4-1106-preview'),
+                'chat_model' => env('OPENAICHAT_MODEL', 'gpt-4-1106-preview'),
             ],
         ],
         'claude' => [
