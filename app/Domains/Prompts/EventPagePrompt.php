51d554a97b761e3c9ba08071a14afee9661c9279
Alfred Nutile <alfrednutile@gmail.com>
2024-07-22 07:41:43 -0400

    add new prompt template for event data

--- app/Domains/Prompts/EventPagePrompt.php
+++ app/Domains/Prompts/EventPagePrompt.php
@@ -12,26 +12,50 @@ class EventPagePrompt
 
         return <<<PROMPT
 <ROLE>
-You are an assistant helping to get event data from a website.
+You are an AI assistant tasked with extracting event data from website content.
 
+<INSTRUCTIONS>
+1. Analyze the provided website HTML content below the <CONTENT> tag.
+2. Look for information about events within the content.
+3. If no event data is found, respond with a single word: false
+4. If event data is found, extract the following information for each event:
+   - Event Title
+   - Start Date
+   - End Date
+   - Location
+   - Description
+   - Any other relevant data
+5. Format the extracted data as a JSON array according to the specifications below.
 
-<TASKS>
-If the site has no data about events just return false. Else
-return the Event Title, then Start Date, End Date, Location, Description, and any other relevant data.
+<OUTPUT_FORMAT>
+If events are found, return a JSON array with the following structure:
 
-<FORMAT>
-On a non false response you will return the following:
-Title: Event Title
-Start Date: Start Date
-End Date: End Date
-Location: Location
-Description: Description
-<END FORMAT>
+[
+  {
+    "title": "Event Title",
+    "startDate": "Start Date",
+    "endDate": "End Date",
+    "location": "Location",
+    "description": "Description",
+    "additionalInfo": "Any other relevant data"
+  },
+  {
+    "title": "Event Title",
+    "startDate": "Start Date",
+    "endDate": "End Date",
+    "location": "Location",
+    "description": "Description",
+    "additionalInfo": "Any other relevant data"
+  }
+]
 
-** WEBSITE HTML IS BELOW THIS LINE **
+If no events are found, return an empty JSON array: []
+
 <CONTENT>
-
 $context
+</CONTENT>
+
+Respond only with the JSON array or 'false' if no events are found. Do not include any explanations or additional text in your response.
 
 PROMPT;
     }
