df52ccee69c6780387bbd41f9aa720e7966bda4a
Alfred Nutile <alfrednutile@gmail.com>
2024-07-26 14:20:48 -0400

    templatizer

---
+++ app/Domains/Tokenizer/Templatizer.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Domains\Tokenizer;
+
+
+class Templatizer
+{
+
+    protected string $content;
+    protected array $token;
+    protected string|null $replacement;
+
+    protected bool $appendContext = false;
+
+    public static function getTokens(): array
+    {
+        $reflection = new \ReflectionClass(Templatizer::class);
+        $methods = $reflection->getMethods(\ReflectionMethod::IS_PROTECTED | \ReflectionMethod::IS_PUBLIC);
+
+        return array_values(array_filter(array_map(function($method) {
+            $name = $method->getName();
+            if (!in_array($name, ['makeMethod', 'handle', 'getTokens'])) {
+                return strtoupper($name);
+            }
+            return null;
+        }, $methods)));
+    }
+
+    public function handle(
+        string $content,
+        array $token = [],
+        string|null $replacement = null): string
+    {
+
+        if ($this->appendContext && !str($content)->contains('[CONTEXT]')) {
+            $content = str($content)->append('[CONTEXT]')->toString();
+        }
+
+        $this->content = $content;
+        $this->token = (empty($token)) ? self::getTokens() : $token;
+        $this->replacement = $replacement;
+
+        foreach ($this->token as $token) {
+            $method = $this->makeMethod($token);
+            if (method_exists($this, $method)) {
+                $this->{$method}();
+            }
+        }
+
+        return $this->content;
+
+    }
+
+
+    protected function makeMethod(string $token): string {
+        return str($token)->remove(['[', ']'])->lower()->snake()->toString();
+    }
+
+    protected function year() {
+        $replacement = now()->format('Y');
+
+        $this->content =
+            str($this->content)
+                ->replace('[YEAR]', $replacement)
+                ->toString();
+    }
+
+    protected function end_week() {
+        $replacement = now()->endOfWeek()->format('m/d/Y');
+        $replacement = $replacement . " " . now()->endOfWeek()->format('M d, Y');
+        $this->content =
+            str($this->content)
+                ->replace('[END_WEEK]', $replacement)
+                ->toString();
+    }
+
+    public function appendContext(bool $appendContext = false): self
+    {
+        $this->appendContext = $appendContext;
+        return $this;
+    }
+
+
+    protected function start_week() : void {
+        $replacement = now()->startOfWeek()->format('m/d/Y');
+        $replacement = $replacement . " " . now()->startOfWeek()->format('M d, Y');
+        $this->content =
+            str($this->content)
+                ->replace('[START_WEEK]', $replacement)
+            ->toString();
+    }
+
+    protected function next_month() : void {
+        $replacement = now()->addMonth()->format('m/y');
+        $replacement = $replacement . " " . now()->addMonth()->format('M  Y');
+        $this->content =
+            str($this->content)
+                ->replace('[NEXT_MONTH]', $replacement)
+            ->toString();
+    }
+
+    protected function current_month() : void {
+        $replacement = now()->format('m/y');
+        $replacement = $replacement . " " . now()->format('M  Y');
+        $this->content =
+            str($this->content)
+                ->replace('[CURRENT_MONTH]', $replacement)
+            ->toString();
+    }
+
+    protected function today() : void {
+        $replacement = now()->format('m/d/Y');
+        $replacement = $replacement . " " . now()->format('M d, Y');
+        $this->content =
+            str($this->content)
+                ->replace('[START_WEEK]', $replacement)
+                ->toString();
+    }
+
+    protected function user_input() : void{
+        $this->content =  str($this->content)->replace('[USER_INPUT]', $this->replacement)->toString();
+    }
+
+    protected function context() : void{
+        $this->content =  str($this->content)->replace('[CONTEXT]', $this->replacement)->toString();
+    }
+
+}
---
+++ tests/Feature/TemplatizerTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Feature;
+
+use App\Domains\Tokenizer\Templatizer;
+use Tests\TestCase;
+
+class TemplatizerTest extends TestCase
+{
+    public function test_context() {
+        $templatizer = new Templatizer();
+        $context = $templatizer->handle(
+            'Hello [CONTEXT]',
+            [
+                '[CONTEXT]'
+            ],
+            'World!'
+        );
+
+        $this->assertEquals('Hello World!', $context);
+    }
+
+    public function test_default_tokens() {
+        $replace = now()->startOfWeek()->format('m/d/Y');
+        $replace2 = now()->endOfWeek()->format('M d, Y');
+        $templatizer = new Templatizer();
+        $context = $templatizer->handle(
+            'Hello [START_WEEK] to [END_WEEK] [CONTEXT]',
+            []
+            ,
+            'World!'
+        );
+
+        $this->assertStringContainsString($replace, $context);
+        $this->assertStringContainsString($replace2, $context);
+        $this->assertStringContainsString('World', $context);
+    }
+
+    public function test_append_context() {
+        $templatizer = new Templatizer();
+
+        $context = $templatizer->appendContext(true)
+            ->handle(
+            'Hello [START_WEEK] to [END_WEEK]',
+            [],
+            'World!',
+            true
+        );
+        $this->assertStringContainsString('World', $context);
+
+        $context = $templatizer->handle(
+                'Hello [START_WEEK] to [END_WEEK]',
+                [],
+                'World!',
+                true
+            );
+        $this->assertStringNotContainsString('World', $context);
+    }
+
+    public function test_date_range() {
+        $replace = now()->startOfWeek()->format('m/d/Y');
+        $replace2 = now()->startOfWeek()->format('M d, Y');
+        $templatizer = new Templatizer();
+        $context = $templatizer->handle(
+            'Hello [START_WEEK] to [END_WEEK]',
+            [
+                '[START_WEEK]',
+                '[END_WEEK]'
+            ]
+            ,
+            'World!'
+        );
+
+        $this->assertStringContainsString($replace, $context);
+        $this->assertStringContainsString($replace2, $context);
+    }
+
+    public function test_get_tokens() {
+        $tokens = Templatizer::getTokens();
+        $this->assertIsArray($tokens);
+        $this->assertTrue(in_array('CONTEXT', $tokens));
+        $this->assertTrue(in_array('START_WEEK', $tokens));
+        $this->assertTrue(in_array('END_WEEK', $tokens));
+    }
+}
