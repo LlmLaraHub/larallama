9aa68e0d1c61d3d8c9a56fd845bb6cb906386a7b
Alfred Nutile <alfrednutile@gmail.com>
2024-07-26 15:19:46 -0400

    add tokens

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Domains\Tokenizer\Templatizer;
 use App\Models\Setting;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
@@ -62,6 +63,7 @@ class HandleInertiaRequests extends Middleware
                 return Feature::all();
             }),
             'tools' => LlmDriverFacade::getFunctionsForUi(),
+            'tokens' => Templatizer::getTokens(),
         ]);
     }
 }
