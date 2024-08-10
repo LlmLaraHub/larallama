321641ffe3e03adb3379772f2d765020bfa60614
Alfred Nutile <alfrednutile@gmail.com>
2024-08-09 20:55:46 -0400

    remove those no longer needed modules

--- Modules/EmailSource/resources/views/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('emailsource::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>Module: {!! config('emailsource.name') !!}</p>
-@endsection
--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
         laravel({
             input: ['resources/js/app.js'],
             refresh: true,
-            detectTls: 'larachain.test',
         }),
         vue({
             template: {
