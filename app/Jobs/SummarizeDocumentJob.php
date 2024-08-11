8c1bf674447103d18968079dccc0eeccc9c6475e
Alfred Nutile <alfrednutile@gmail.com>
2024-08-10 20:09:14 -0400

    THere might have been a bug here and I moved it to Chat

--- app/Jobs/SummarizeDocumentJob.php
+++ app/Jobs/SummarizeDocumentJob.php
@@ -17,7 +17,9 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
 use Laravel\Pennant\Feature;
+use LlmLaraHub\LlmDriver\Functions\ToolTypes;
 use LlmLaraHub\LlmDriver\LlmDriverFacade;
+use LlmLaraHub\LlmDriver\Requests\MessageInDto;
 use LlmLaraHub\LlmDriver\Responses\CompletionResponse;
 
 class SummarizeDocumentJob implements ShouldQueue
@@ -61,52 +63,39 @@ class SummarizeDocumentJob implements ShouldQueue
 
         $content = implode(' ', $content);
 
-        Log::info('[LaraChain] Summarizing Document', [
-            'token_count_v2' => token_counter_v2($content),
-            'token_count_v1' => token_counter($content),
-        ]);
+
 
         if (empty($this->prompt)) {
             $prompt = SummarizeDocumentPrompt::prompt($content);
         } else {
             $prompt = Templatizer::appendContext(true)
                 ->handle($this->prompt, $content);
-
         }
+        Log::info('[LaraChain] Summarizing Document', [
+            'token_count_v2' => token_counter_v2($content),
+            'token_count_v1' => token_counter($content),
+            'prompt' => $prompt,
+        ]);
 
         /** @var CompletionResponse $results */
         $results = LlmDriverFacade::driver(
             $this->document->getDriver()
-        )->completion($prompt);
+        )->setToolType(ToolTypes::NoFunction)
+            ->chat([
+                MessageInDto::from([
+                    'content' => $prompt,
+                    'role' => 'user',
+                ])
+            ]);
+
+        Log::info('[LaraChain] Summarizing Document Results', [
+            'results' => $results->content,
+        ]);
 
         $this->results = $results->content;
 
-        if (Feature::active('verification_prompt_summary')) {
-
-            $verifyPrompt = <<<'PROMPT'
-            This the content from all the documents in this collection.
-            Then that was passed into the LLM to summarize the results.
-            PROMPT;
-
-            $dto = VerifyPromptInputDto::from(
-                [
-                    'chattable' => $this->document->collection,
-                    'originalPrompt' => $prompt,
-                    'context' => $content,
-                    'llmResponse' => $this->results,
-                    'verifyPrompt' => $verifyPrompt,
-                ]
-            );
-
-            /** @var VerifyPromptOutputDto $response */
-            $response = VerifyResponseAgent::verify($dto);
-
-            $this->results = $response->response;
-
-        }
-
         $this->document->update([
-            'summary' => $this->results,
+            'summary' => $results->content,
             'status_summary' => StatusEnum::SummaryComplete,
         ]);
 
